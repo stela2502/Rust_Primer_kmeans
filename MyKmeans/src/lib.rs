23227a0504e025d5ad0a0de7b4ea0799d2beb1a0
Stefan Lang <Stefan.Lang@med.lu.se>
2022-12-19 12:29:56 +0100

    Almost got the data in...

--- MyKmeans/src/lib.rs
+++ MyKmeans/src/lib.rs
@@ -1,4 +1,45 @@
 use rand::prelude::*;
+use std::error::Error;
+use csv::ReaderBuilder;
+
+//https://docs.rs/csv/latest/csv/
+
+pub fn read_tsv( fname:&str, ncol: usize, nrow: usize ) ->  Result< Vec<Vec<f32>>, &str>  {
+    // Build the CSV reader and iterate over each record.
+
+    let rdr = ReaderBuilder::new()
+    	.delimiter(b'\t')
+    	.from_path( fname );
+
+    let mut i = 0;
+
+    let mut res: Vec<Vec<f32>> = vec!( vec!(0.0; nrow); ncol );
+
+    if let mut records = rdr.expect("REASON").records(){
+    	while let Some(result) = records.next() {
+	        match result{
+	        	Ok(res) => {
+	        		println!("I got one entry: {:?}", res.pop());
+	        		
+	        		res
+	        	},
+	        	Err(err) => {
+	        		eprintln!("I got an error: {:?}", err);
+	        		return Err::<Vec<Vec<f32>>, &str>("record unusable")
+	        	}
+	        };
+	        //eprintln!("What have I got here? {}", record.len() );
+	        i +=1;
+	    }
+    }else {
+    	eprintln!("I expected at least one data line!" );
+    }
+     
+
+    println!("I have read {} lines",i);
+    return Ok(res);
+}
+
 
 fn e_dist( a:&Vec<f32>, b:&Vec<f32> ) -> f32{
 	let mut res:f32 = 0.0;
@@ -9,7 +50,7 @@ fn e_dist( a:&Vec<f32>, b:&Vec<f32> ) -> f32{
 	return res;
 }
 
-
+/*
 pub fn kmeans( data:&Vec<Vec<f32>>, k:usize, it:usize ) -> Vec<usize> {
 	// implement the thing here
 	// 1. where do we store the data?
@@ -34,7 +75,7 @@ pub fn kmeans( data:&Vec<Vec<f32>>, k:usize, it:usize ) -> Vec<usize> {
 		}
 	}
 }
-
+*/
 
 #[cfg(test)]
 mod tests {
@@ -49,4 +90,10 @@ mod tests {
     	let result = e_dist( &a ,&b );
         assert_eq!(result, 3.0 as f32);
     }
+
+    fn t_read_tsv(){
+    	let fname = "TestData/Spellman_Yeast_Cell_Cycle.tsv";
+    	read_tsv( fname );
+    }
+
 }
--- MyKmeans/src/main.rs
+++ MyKmeans/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+use MyKmeans;
+
+/// Split a pair of BD rhapsody fastq files (R1 and R2) into sample specific fastq pairs
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Stefan L. <stefan.lang@med.lu.se>")]
+struct Opts {
+    /// the data you want to cluster
+    #[clap(short, long)]
+    file: String,
+    /// how many clusters to create
+    #[clap(short, long)]
+    k: usize,
+    /// the file to store the clusters in
+    #[clap(short, long)]
+    outfile: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let opts: Opts = Opts::parse();
+
+    let data = MyKmeans::read_tsv( &opts.file, 17, 256 );
+
 }
